1f2ba03d90f0449db3e73eb9fae940bb82ba0059
Mark Larah <mark@larah.me>
2017-09-20 01:03:05 -0700

    iterm

--- ansible/roles/macos/hyper/files/.hyper.js
+++ ansible/roles/macos/hyper/files/.hyper.js
@@ -8,7 +8,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Source Code Pro',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
@@ -44,36 +44,37 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    // padding: '12px 14px',
 
+    colors: [],
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
+    // colors: {
+    //   black: '#000000',
+    //   red: '#ff0000',
+    //   green: '#33ff00',
+    //   yellow: '#ffff00',
+    //   blue: '#0066ff',
+    //   magenta: '#cc00ff',
+    //   cyan: '#00ffff',
+    //   white: '#d0d0d0',
+    //   lightBlack: '#808080',
+    //   lightRed: '#ff0000',
+    //   lightGreen: '#33ff00',
+    //   lightYellow: '#ffff00',
+    //   lightBlue: '#0066ff',
+    //   lightMagenta: '#cc00ff',
+    //   lightCyan: '#00ffff',
+    //   lightWhite: '#ffffff'
+    // },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     // make sure to use a full path if the binary name doesn't work
     // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
     // if you're using powershell, make sure to remove the `--login` below
-    shell: '',
+    shell: 'zsh',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
@@ -86,7 +87,7 @@ module.exports = {
     bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false
+    copyOnSelect: false,
 
     // if true, on right click selected text will be copied or pasted if no
     // selection is present (true by default on Windows)
@@ -104,13 +105,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyper-chesterish',
-    'hyper-statusline',
-  ],
+  plugins: ['hyperterm-gruvbox-dark'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
 };
