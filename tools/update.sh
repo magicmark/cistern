bc76c07d27b540107da14ecb7d58b48ec34a4e7b
Mark Larah <mark@larah.me>
2017-12-07 13:31:30 -0800

    add crontab

---
+++ tools/update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -euo pipefail
+
+# https://stackoverflow.com/a/246128/4396258
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CISTERN="$(dirname "${SCRIPT_DIR}")"
+
+pushd $CISTERN
+git pull origin master
+make run
